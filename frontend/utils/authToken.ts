e70616d955e29c0c193ff016376e155c5818b221
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2025-12-17 17:28:35 +0100

    Update authToken.ts

--- frontend/utils/authToken.ts
+++ frontend/utils/authToken.ts
@@ -2,7 +2,6 @@ let accessToken: string | null = null;
 
 export function setAccessToken(token: string | null) {
   accessToken = token;
-  console.log("Access token set:", accessToken);
 }
 
 export function getAccessToken() {
